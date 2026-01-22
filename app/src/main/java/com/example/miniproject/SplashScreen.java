458b0b91f594230dbaec28ae43691c38096f607c
Jatin Bhatu <j.h.bhatu1477@gmail.com>
2026-01-23 00:14:45 +0530

    Jatin UI have page: 1)splashscreen, 2)start, 3)login, 4)Otp

--- app/src/main/java/com/example/miniproject/SplashScreen.java
+++ app/src/main/java/com/example/miniproject/SplashScreen.java
@@ -1,24 +1,37 @@
 package com.example.miniproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login); // ← YOUR LOGIN XML
-
+        setContentView(R.layout.activity_splash_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, StartActivity.class);
+                startActivity(intent);
+                finish(); // close splash activity
+            }
+        }, 1000);
     }
 }
--- app/src/main/java/com/example/miniproject/StartActivity.java
+++ app/src/main/java/com/example/miniproject/StartActivity.java
@@ -1,24 +1,43 @@
 package com.example.miniproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.widget.Button;
+import android.view.View;
+
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity {
+
+    Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login); // ← YOUR LOGIN XML
-
+        setContentView(R.layout.activity_start);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnNext = findViewById(R.id.btnnext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
         });
     }
 }
