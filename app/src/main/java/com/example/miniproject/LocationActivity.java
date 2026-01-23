63a03fd70d82a607a31ca47bc20361837dca1b32
Jatin Bhatu <j.h.bhatu1477@gmail.com>
2026-01-23 11:49:55 +0530

    just commit

--- app/src/main/java/com/example/miniproject/LocationActivity.java
+++ app/src/main/java/com/example/miniproject/LocationActivity.java
@@ -1,11 +1,6 @@
 package com.example.miniproject;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import android.widget.Button;
-import android.view.View;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,31 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class StartActivity extends AppCompatActivity {
-
-    Button btnNext;
+public class LocationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_location);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.backlocation), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        btnNext = findViewById(R.id.btnnext);
-
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
-                startActivity(intent);
-
-            }
         });
     }
 }
--- app/src/main/java/com/example/miniproject/LoginActivity.java
+++ app/src/main/java/com/example/miniproject/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.miniproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -21,7 +20,7 @@ public class LoginActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login); // ← YOUR LOGIN XML
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.backlocation), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
